7c295dae119b680c5aa7d82abfd71b593986e703
sakshamnagpall <saksham1930.be21@icloud.com>
2024-04-24 10:56:28 +0530

    Added Project

---
+++ EzyFitness_Decoders/EzyFitness_Decoders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EzyFitness_Decoders
+//
+//  Created by student on 24/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
